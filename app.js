db4ff47f4154c276e0104961fd7ffc8f0f01cf44
Akash Hardia <aakashhardiya@gmail.com>
2018-07-09 05:28:38 +0000

    Loading Addstudent.ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app     = express();
 
 app.use(express.static('public'));
+app.use(express.static('vendors'));
 app.use(express.static('src'));
 app.set('view engine','ejs');
 app.set('views','./src/views');
@@ -9,6 +10,10 @@ app.set('views','./src/views');
 
 app.get('/', function(req,res){
    res.render('index'); 
+});
+
+app.get('/add/student',function(req,res){
+   res.render('addstudent'); 
 });
 
 app.listen(process.env.PORT,process.env.IP,function(err){
