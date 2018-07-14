6b0c0a99ff97cc9155e303d57a587457753b6c99
annant05 <annantguptaneema@gmail.com>
2018-07-14 12:08:21 +0530

    bug fixed. Data is displayed in console.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express"),
     app = express();
-    
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
     extended: true
@@ -33,37 +33,27 @@ app.get('/form', function (req, res) {
 app.post('/add-student', function (req, res) {
     console.log(req.body.student.firstname);
     console.log(req.body.student.lastname);
-    // console.log(req.body.student.gender);
-    // console.log(req.body.student.dob);
-    //
-    // console.log("\n\n Father \n");
-    //
-    // console.log(req.body.father.name);
-    // console.log(req.body.father.email);
-    // console.log(req.body.father.mobile);
-    //
-    // console.log("\n\n mother \n");
-    // console.log(req.body.mother.name);
-    // console.log(req.body.mother.email);
-    // console.log(req.body.mother.mobile);
+    console.log(req.body.student.gender);
+    console.log(req.body.student.dob);
+
+    console.log("\n\n Father \n");
+
+    console.log(req.body.father.name);
+    console.log(req.body.father.email);
+    console.log(req.body.father.mobile);
+
+    console.log("\n\n mother \n");
+    console.log(req.body.mother.name);
+    console.log(req.body.mother.email);
+    console.log(req.body.mother.mobile);
 
     res.end("got your data");
 });
 
 
-
-
-
-
-
-
-
-
-
-
-app.listen(  8032 /*process.env.PORT*/ ,/*process.env.IP,*/ function(err){
-    if(err) console.log("There was some problem in starting the server  : " +  JSON.stringify(err,undefined,2));
-    else    console.log('server started on port : ' + "8032"  /*process.env.PORT + '  and  ' + process.env.IP*/);
+app.listen(8032 /*process.env.PORT*/, /*process.env.IP,*/ function (err) {
+    if (err) console.log("There was some problem in starting the server  : " + JSON.stringify(err, undefined, 2));
+    else console.log('server started on port : ' + "8032"  /*process.env.PORT + '  and  ' + process.env.IP*/);
 });
 
 // app.listen(8032, function (err) {
